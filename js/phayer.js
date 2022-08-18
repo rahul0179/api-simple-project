985088025f03612ea8a1e0a96285ca639e60d0f4
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-19 02:23:13 +0600

    sportsdb

--- js/phayer.js
+++ js/phayer.js
@@ -1,1 +1,38 @@
-console.log(0k)
+
+//  all sports show home page 
+
+const allSports = () => {
+    const url = `https://www.thesportsdb.com/api/v1/json/2/all_sports.php`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayAllSports(data))
+
+}
+allSports();
+const displayAllSports = data => {
+
+    const sports = data.sports
+    console.log(sports)
+    const tableData = document.getElementById("table-data")
+    sports.forEach(i => {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <th scope="row">${i.idSport}</th>
+        <td>${i.strSport}</td>
+        <td>${i.strFormat}</td>
+        <td><img src="${i.strSportThumb}" alt=""></td>
+        <td><img src="${i.strSportIconGreen}" alt=""></td>
+
+        
+        `
+        tableData.appendChild(tr)
+
+
+
+
+
+    });
+
+
+}
+
